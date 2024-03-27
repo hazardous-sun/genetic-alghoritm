6a6a4d00667c80d90df5a83781f7fea25f50f3e5
solaire-notebook <contact@unharmfulhazard.com>
2024-03-27 19:28:43 -0300

    Added missing parameter for setting the amount of generations

--- Source Files/GeneticAlgorithmTest.cpp
+++ Source Files/GeneticAlgorithmTest.cpp
@@ -5,9 +5,10 @@
 int main() {
     std::cout << "Hi, student!\n";
     int populationSize = 100;
+    int generations = 10;
     int bits = 8;
     int low = 0;
     int high = 4;
-    GenericAlgorithm ga(100);
+    GenericAlgorithm ga(populationSize, generations);
     Solution bestSolution = ga.run(bits, low, high);
 }
