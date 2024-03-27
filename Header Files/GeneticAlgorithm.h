a7946cb8631586c4c13c914ffdacea845654e4ee
solaire-notebook <contact@unharmfulhazard.com>
2024-03-27 19:50:53 -0300

    Started to add the logic for the tournament crossover, the actual crossover is still missing

--- Header Files/GeneticAlgorithm.h
+++ Header Files/GeneticAlgorithm.h
@@ -4,11 +4,17 @@
 
 class GenericAlgorithm {
 public:
-    GenericAlgorithm(int populationSize, int generations);
+    GenericAlgorithm(int populationSize, int generations, int tournamentGroupSize);
 
     Solution run(int numberOfBits, int low, int high);
 
 private:
+    std::vector<Solution> tournamentCrossover(std::vector<Solution> const &currentGeneration);
+
+    Solution tournamentWinner(std::vector<Solution> const &currentGeneration);
+
+private:
     int mPopulationSize;
     int mGenerations;
+    int mTournamentGroupSize;
 };
--- Source Files/GeneticAlgorithm.cpp
+++ Source Files/GeneticAlgorithm.cpp
@@ -1,11 +1,14 @@
 #include "../Header Files/GeneticAlgorithm.h"
+#include "../Header Files/Utils.h"
 
 #include <ctime>
 #include <iostream>
+#include <set>
 
-GenericAlgorithm::GenericAlgorithm(int populationSize, int generations) :
+GenericAlgorithm::GenericAlgorithm(int populationSize, int generations, int tournamentGroupSize) :
         mPopulationSize(populationSize),
-        mGenerations(generations) {
+        mGenerations(generations),
+        mTournamentGroupSize(tournamentGroupSize) {
 
 }
 
@@ -24,7 +27,44 @@ Solution GenericAlgorithm::run(int numberOfBits, int low, int high) {
         std::cout << s.toString() << "\n";
     }
 
-    for (int i = 0; i < mGenerations; i++) {}
+    for (int i = 0; i < mGenerations; i++) {
+        std::vector<Solution> crossedSolutions = tournamentCrossover();
+    }
 
     return best;
+}
+
+Solution GenericAlgorithm::tournamentWinner(std::vector<Solution> const &currentGeneration) {
+    std::vector<Solution> candidates;
+    std::set<int> randomNumbers = randomDistinctNumbers(mPopulationSize, mTournamentGroupSize);
+
+    for (int rn: randomNumbers) {
+        candidates.push_back(currentGeneration[rn]);
+    }
+
+    Solution bestCandidate = candidates[0];
+    double maxFitness = candidates[0].fitness();
+
+    for (Solution s: candidates) {
+        double fitness = s.fitness();
+
+        if (fitness > maxFitness) {
+            maxFitness = fitness;
+            bestCandidate = s;
+        }
+    }
+
+    return bestCandidate;
+}
+
+std::vector<Solution> GenericAlgorithm::tournamentCrossover(std::vector<Solution> const &currentGeneration) {
+    std::vector<Solution> newSolution;
+    while (newSolution.size() < mPopulationSize) {
+        // Get candidates
+        Solution winner1 = tournamentWinner(currentGeneration);
+        Solution winner2 = tournamentWinner(currentGeneration);
+
+        // Perform crossover
+        std::vector<Solution> children = singlePointCrossover(winner1, winner2);
+    }
 }
