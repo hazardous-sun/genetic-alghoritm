e0f9486f399b35270f49b65895e2e5820de0415b
solaire-notebook <contact@unharmfulhazard.com>
2024-03-27 18:10:16 -0300

    Added functions to display the Solution and to transform bits into doubles

--- Header Files/Solution.h
+++ Header Files/Solution.h
@@ -1,11 +1,20 @@
 #pragma once
 
 #include <vector>
+#include <string>
 
 class Solution {
 public:
-    Solution(int numberOfBits);
+    Solution(int numberOfBits, int low, int high);
+
+    std::string toString() const;
+
+    double fitness() const;
+
 private:
-    int mNumberOfBits;
+    double bitsToDouble() const;
+
+private:
+    int mNumberOfBits, mLow, mHigh;
     std::vector<int> mBits;
 };
--- Source Files/Solution.cpp
+++ Source Files/Solution.cpp
@@ -1,6 +1,12 @@
 #include "../Header Files/Solution.h"
+#include "../Header Files/Utils.h"
+#include <math.h>
+#include <sstream>
 
-Solution::Solution(int numberOfBits): mNumberOfBits(numberOfBits) {
+Solution::Solution(int low, int high, int numberOfBits) :
+        mLow(low),
+        mHigh(high),
+        mNumberOfBits(numberOfBits) {
     /*
      reserve() is a function that allocates a memory space of size N to the std::vector
 
@@ -9,4 +15,29 @@ Solution::Solution(int numberOfBits): mNumberOfBits(numberOfBits) {
      well as optimize the runtime, since there won't be any further reallocation.
      */
     mBits.reserve(numberOfBits);
+
+    for (int i = 0; i < numberOfBits; i++) {
+        mBits.push_back(rand() % 2);
+    }
+}
+
+std::string Solution::toString() const {
+    std::ostringstream stream;
+    stream << "[";
+    for (int bit: mBits) {
+        stream << bit << " ";
+    }
+    stream << "] double = " << bitsToDouble();
+}
+
+double Solution::bitsToDouble() const {
+    double prec = precision(mNumberOfBits, mLow, mHigh);
+    int sum = 0;
+    for (int i = 0; i < mNumberOfBits; i++) {
+        /*
+         Converts from base 10 to base 2
+         */
+        sum = sum + pow(2, i) * mBits[i];
+    }
+    return sum * prec + mLow;
 }
